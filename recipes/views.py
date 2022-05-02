3040a29ddfca52d0053a9559833dfaeb4e427a8c
shellieD <shellie.downie13@gmail.com>
2022-05-02 23:01:35 +0000

    Create RecipeView class.

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -8,3 +8,24 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-added_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class RecipeView(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('added_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_view.html",
+            {
+                "recipe": recipe,
+                "commnets": comments,
+                "liked": liked
+            }
+        )
