015560e01b116c9706826e72fbf269bf4f7cec61
shellieD <shellie.downie13@gmail.com>
2022-05-03 15:21:25 +0000

    Style signup, login and logout pages.

--- holy_shiitake/settings.py
+++ holy_shiitake/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms'
     'recipes',
 ]
 
@@ -56,6 +57,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
