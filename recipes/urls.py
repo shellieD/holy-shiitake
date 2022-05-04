187d89e31be78390c411eb752040d0eddea318c1
shellieD <shellie.downie13@gmail.com>
2022-05-04 11:33:29 +0000

    Add path to RecipeLike view.

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('<slug:slug>/', views.RecipeView.as_view(), name='recipe_view'),
+    path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
 ]
