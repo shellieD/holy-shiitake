57ad06191559b319de938e46c1180dc8ac74d03c
shellieD <shellie.downie13@gmail.com>
2022-05-05 22:07:44 +0000

    Add RecipeForm class

--- recipes/forms.py
+++ recipes/forms.py
@@ -3,6 +3,20 @@ from django import forms
 
 
 class CommentForm(forms.ModelForm):
+    """Comment Form """
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class RecipeForm(forms.ModelForm):
+    """Recipe Form"""
+    class Meta:
+        model = Recipe
+        fields = (
+            'recipe_name',
+            'description',
+            'ingredients',
+            'method',
+            'recipe_image'
+        )
