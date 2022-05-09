3eba39b0166d95ecac4784015b5ec98ad2b69fc2
shellieD <shellie.downie13@gmail.com>
2022-05-09 22:26:40 +0000

    Add path to user recipes page.

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,12 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
-    path('recipes/<slug:slug>/', views.RecipeView.as_view(), name='recipe_view'),
+    path(
+        'recipes/<slug:slug>/', views.RecipeView.as_view(), name='recipe_view'
+    ),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
-    path('recipes/add', views.AddRecipe.as_view(), name="add_recipe")
+    path('recipes/add', views.AddRecipe.as_view(), name="add_recipe"),
+    path(
+        'recipes/my_recipes', views.UserRecipes.as_view(), name='user_recipes'
+    )
 ]
