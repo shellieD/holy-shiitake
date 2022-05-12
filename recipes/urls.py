ea1f0bb079b2365d34abb22ea11dd1fa50cfb32b
shellieD <shellie.downie13@gmail.com>
2022-05-12 00:19:35 +0000

    Add path to update_recipes view.

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,13 +3,34 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='home'),
     path(
-        'recipes/<slug:slug>/', views.RecipeView.as_view(), name='recipe_view'
+        '',
+        views.RecipeList.as_view(),
+        name='home'
     ),
-    path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
-    path('recipes/add', views.AddRecipe.as_view(), name="add_recipe"),
     path(
-        'recipes/my_recipes', views.UserRecipes.as_view(), name='user_recipes'
-    )
+        'recipes/<slug:slug>/',
+        views.RecipeView.as_view(),
+        name='recipe_view'
+    ),
+    path(
+        'like/<slug:slug>',
+        views.RecipeLike.as_view(),
+        name='recipe_like'
+    ),
+    path(
+        'recipes/add',
+        views.AddRecipe.as_view(),
+        name="add_recipe"
+    ),
+    path(
+        'recipes/my_recipes',
+        views.UserRecipes.as_view(),
+        name='user_recipes'
+    ),
+    path(
+        'recipes/update_recipe/<slug:slug>',
+        views.UpdateRecipe.as_view(),
+        name='update_recipe'
+    ),
 ]
