c1cdf3abcdae16cd2b30cae57d4f435d721c5e89
shellieD <shellie.downie13@gmail.com>
2022-05-28 13:25:53 +0000

    Add view for custom 500 error.

--- holy_shiitake/views.py
+++ holy_shiitake/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def custom_404_error(request, exception):
     """Custom 404 Error View"""
     return render(request, '404.html')
+
+
+def custom_500_error(request, exception):
+    """Custom 500 Error View"""
+    return render(request, '500.html')
